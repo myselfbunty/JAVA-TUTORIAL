34a809ac05bd0a746e6086961e14cf81395bd98a
myselfbunty <buntykumar1444@gmail.com>
2022-07-17 22:02:30 +0530

    if conditions

---
+++ If.java
@@ -0,0 +1,14 @@
+public class If {
+    public static void main (String[] args){
+        int age = 12;
+        if(age>18){
+            System.out.println("You can vote");
+        }
+        else if(age == 18){
+            System.out.println("You vote");
+        }
+        else{
+            System.out.println("You can not  vote");
+        }
+    }
+}
