4cfb95dbc480a49503edd1b3553f8400211707fd
myselfbunty <109450349+myselfbunty@users.noreply.github.com>
2022-07-26 15:03:50 +0530

    Add files via upload

---
+++ patternQ5.java
@@ -0,0 +1,16 @@
+public class patternQ5 {
+    public static void main(String args[]){
+        int n=4;
+        for(int i=1; i <=n; i++){
+            //inner loop space print
+            for(int j=1; j <=n-i; j++){
+                System.out.print(" ");
+            }
+            // inner loop for star print
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
